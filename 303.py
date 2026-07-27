904852185a6336c6f8f4b9c004b621b55f349314
jsujith644-sys <jsujith644@gmail.com>
2026-07-27 12:06:09 +0530

    Add files via upload

---
+++ 303.py
@@ -0,0 +1,15 @@
+class NumArray(object):
+
+    def __init__(self, nums):
+        self.nums = nums
+        
+
+    def sumRange(self, left, right):
+        summ = sum(self.nums[left:right+1])
+        return summ
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
