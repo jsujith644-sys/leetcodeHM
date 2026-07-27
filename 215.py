73f222ff162ce244e5c2961ca6cafe284b3391eb
jsujith644-sys <jsujith644@gmail.com>
2026-07-27 09:53:18 +0530

    Add files via upload

---
+++ 215.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        x=nums
+        x.sort(reverse=True)
+        return x[k-1]
